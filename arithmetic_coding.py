3c32cc0fc6b04c182e1bd16dbd28c140f92aa720
Nils Imdahl <imdahlnils@gmail.com>
2023-05-03 23:27:57 +0200

    infinite precision decoder

--- arithmetic_coding.py
+++ arithmetic_coding.py
@@ -3,18 +3,18 @@ import numpy as np
 
 
 class ArithmeticCoding:
-    def encode(self, symbols, model):
+    def __init__(self, model):
+        self.model = model
+
+    def encode(self, symbols):
         lb, ub = 0, 1
         for symbol in symbols:
-            p1, p2 = model.predict(symbol)
+            p1, p2 = self.model.predict(symbol)
             interval = ub - lb
             ub = lb + interval * p2
             lb = lb + interval * p1
-            print(lb, ub)
 
-        print(ub)
-        print(lb)
-
+        """
         # binary search
         def _binary_search(low, high):
             m = (low + high) / 2
@@ -25,9 +25,21 @@ class ArithmeticCoding:
             return ""
 
         return _binary_search(0, 1)
+        """
+        return (ub + lb) / 2
 
-    def decode(self, probs, encoded):
-        pass
+    def decode(self, num_encoded, length):
+        lb, ub = 0, 1
+        res = ""
+        for i in range(length):
+            interval = ub - lb
+            c = model.getSymbol((num_encoded - lb) / interval)
+            res += c
+            p1, p2 = model.predict(c)
+            ub = lb + interval * p2
+            lb = lb + interval * p1
+
+        return res
 
 
 class DummyModel:
@@ -37,14 +49,21 @@ class DummyModel:
             k: (ranges[i], ranges[i + 1])
             for k, i in zip(prob_dict.keys(), range(len(ranges) - 1))
         }
-        print(self.mapping)
+        self.reverse_mapping = {v: k for k, v in self.mapping.items()}
 
     def predict(self, c):
         return self.mapping[c]
 
+    def getSymbol(self, p):
+        for k in self.reverse_mapping.keys():
+            if k[0] <= p < k[1]:
+                return self.reverse_mapping[k]
+
 
 if __name__ == "__main__":
-    ac = ArithmeticCoding()
     model = DummyModel({"H": 0.2, "E": 0.2, "L": 0.4, "O": 0.2})
-    encoded_str = ac.encode("HELLO", model)
-    print(encoded_str)
+    ac = ArithmeticCoding(model)
+    encoded_str = ac.encode("HELLO")
+    print(f"Encoded string: {encoded_str}")
+    decoded_str = ac.decode(encoded_str, 5)
+    print(f"Decoded string: {decoded_str}")
