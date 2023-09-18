29a37728070f3658387b74429b8a8ae87f480559
Nils Imdahl <imdahlnils@gmail.com>
2023-09-19 01:19:47 +0200

    parts of part from notebook

---
+++ CalculatorGPT/model.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MaskedMultiHeadAttention(nn.Module):
+    def __init__(self, cl, n_heads, emb_dim):
+        """
+        We compute the linear transformation of Q,V,K and all heads in
+        parallel (this assumes n_heads*head_size == emb_dim, which is the
+        convention). Secondly we need the Linear Projection back in to the
+        residual path.
+        """
+        super().__init__()
+        self.n_heads = n_heads
+        self.lin_t = nn.Linear(emb_dim, 3 * emb_dim, bias=False)
+        self.proj = nn.Linear(emb_dim, emb_dim)
+        self.flash = hasattr(F, "scaled_dot_product_attention") # use flash attention if available
+        if not self.flash:
+            print("Warning: Flash attention not available, using standard attention")
+            self.register_buffer("tril", torch.tril(torch.ones(cl, cl)))
+
+    def forward(self, x):
+        # Input has Batch (B) and Time (T) and Embedding (C) dimension
+        B, T, C = x.shape
+        q, k, v = self.lin_t(x).split(C, dim=-1)
+        # treat the heads as another Batch dimension
+        q = q.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2)
+        k = k.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2)
+        v = v.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2)
+
+        # use flash attention if pytorch version is high enough
+        if self.flash:
+            out = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        else:
+            wei = q @ k.transpose(-1, -2)
+            wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+            wei *= C**-0.5
+            out = F.softmax(wei, dim=-1) @ v
+
+        # concatenate heads
+        out = out.transpose(1, 2).contiguous().view(B, T, C)
+        return self.proj(out)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.linear = nn.Linear(emb_dim, 4 * emb_dim)
+        self.gelu = nn.GELU()
+        self.proj = nn.Linear(4 * emb_dim, emb_dim)
+
+    def forward(self, x):
+        out = self.gelu(self.linear(x))
+        return self.proj(out)
+
+
+class TransformerDecoderBlock(nn.Module):
+    def __init__(self, cl, n_heads, emb_dim):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(emb_dim)
+        self.mha = MaskedMultiHeadAttention(cl, n_heads, emb_dim)
+        self.ln2 = nn.LayerNorm(emb_dim)
+        self.ff = FeedForward(emb_dim)
+
+    def forward(self, x):
+        x = x + self.mha(self.ln1(x))
+        return x + self.ff(self.ln2(x))
+
+
+class GPT(nn.Module):
+    def __init__(self, n_blocks, vocab_size, cl, n_heads, emb_dim):
+        super().__init__()
+        assert emb_dim % n_heads == 0, "emb_dim not divisible by n_heads"
+        self.cl = cl #needed for generating
+        self.token_emb = nn.Embedding(vocab_size, emb_dim)
+        self.position_emb = nn.Embedding(cl, emb_dim)
+        self.blocks = nn.Sequential(
+            *[TransformerDecoderBlock(cl, n_heads, emb_dim) for _ in range(n_blocks)]
+        )
+        self.lm_head = nn.Linear(emb_dim, vocab_size)
+
+    def forward(self, x, targets=None):
+        B, T = x.shape
+        x = self.token_emb(x) + self.position_emb(torch.arange(T, device=x.device))
+        x = self.blocks(x)
+        logits = self.lm_head(x)
+
+        # calculate loss if targets are provided
+        if targets == None:
+            loss = None
+        else:
+            logits = logits.view(B*T, -1)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, num_tokens=1000):
+        for _ in range(num_tokens):
+            context = idx[:, -self.cl:]
+            logits, _ = self(context)
+            logits = logits[:, -1, :]
+            probs = torch.softmax(logits, dim=-1)
+            next_token = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, next_token), dim=1)
+        return idx
---
+++ CalculatorGPT/train.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python3
+import torch
+
+
+def encode(num1, num2, op, res=None):
+    # encode symbols
+    op_enc = torch.tensor(stoi[op], dtype=torch.long).view(1)
+    equals_enc = torch.tensor(stoi['='], dtype=torch.long).view(1)
+
+    # encode input numbers (left pad with zeros until num_digits)
+    def _encode_num(num, num_digits, reverse=False):
+        if reverse:
+            out = torch.tensor([int(digit) for digit in reversed(str(num).zfill(num_digits))], dtype=torch.long)
+        else:
+            out = torch.tensor([int(digit) for digit in str(num).zfill(num_digits)], dtype=torch.long)
+        return out
+
+    num1_enc = _encode_num(num1, max_digits)
+    num2_enc = _encode_num(num2, max_digits)
+
+    if res == None:
+        out = torch.cat([num1_enc, op_enc, num2_enc, equals_enc])
+    else:
+        res_enc = _encode_num(res, 2*max_digits, True)
+        out = torch.cat([num1_enc, op_enc, num2_enc, equals_enc, res_enc])
+
+    return out
+
+def decode(x):
+    out = []
+    for idx in x:
+        if idx < 10: out.append(str(idx.item())) # if its a digit just add it as a str
+        elif idx == stoi['<END>']: break # END token means we are done
+        else: out.append(itos[idx.item()]) # otherwise encode op
+
+    out =  "".join(out)
+    out = out[:-2*max_digits] + out[:(-2*max_digits)-1:-1] # reverse the result
+    return out
+
+# we sample two random numbers as input and their sum as the label
+def sample_mathproblems(num_problems): 
+    ops = torch.randint(10, 12, (num_problems, ), dtype=torch.long)
+    all_nums = torch.randint(0, 10**max_digits, (num_problems, 2), dtype=torch.long)
+    
+    x = torch.zeros((num_problems, context_length), dtype=torch.long)
+
+    for i, (nums, op) in enumerate(zip(all_nums, ops)):
+        num1, num2 = nums[0].item(), nums[1].item()
+        op_c = itos[op.item()]
+        match op_c:
+            case '+':
+                res = num1 + num2
+            case '-':
+                if num2 > num1: num1, num2 = num2, num1 #swap because we dont want negative numbers
+                res = num1 - num2
+        x[i] = encode(num1, num2, op_c, res)
+
+    input_size = 2*max_digits+2
+    masked_loss = -100 * torch.ones((num_problems, input_size-1), dtype=torch.long)
+    end_token = stoi['<END>'] * torch.ones((num_problems, 1), dtype=torch.long)
+    y = torch.cat([masked_loss, x[:, input_size:], end_token], dim=1)
+    return x, y
+
+
+if __name__ == "__main__":
+    max_digits = 3
+    context_length = 10
+    stoi = {'+': 10, '-':11, '=': 14, '<END>':15}
+    itos = {i: op for op, i in stoi.items()}
